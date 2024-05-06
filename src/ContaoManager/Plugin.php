07567a66fed082e4ad85741bed15367fc227a10e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-06 13:54:46 -0400

    Create Plugin.php

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * @copyright  Bright Cloud Studio
+ * @author     Bright Cloud Studio
+ * @package    Contao CE Glide
+ * @license    LGPL-3.0+
+ * @see	       https://github.com/bright-cloud-studio/contao-popups
+ */
+
+namespace Bcs\MossRegistrationBundle\ContaoManager;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create('Bcs\MossRegistrationBundle\BcsMossRegistrationBundle')
+                ->setLoadAfter(['Contao\CoreBundle\ContaoCoreBundle']),
+        ];
+    }
+}
