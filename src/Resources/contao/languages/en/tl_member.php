1bfd1b90f7ecf4096008d9cab26c2b18ee0b4fae
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-06 14:34:43 -0400

    Create tl_member.php

---
+++ src/Resources/contao/languages/en/tl_member.php
@@ -0,0 +1,7 @@
+<?php
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_member']['price_tier_legend']         = array('Test Legend', 'This is a test legend');
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_member']['price_tier']         = 'Test Field';
