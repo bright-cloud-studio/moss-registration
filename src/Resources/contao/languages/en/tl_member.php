6e748b7481449e54b6362352da135966df1a7c29
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-06 15:27:40 -0400

    Update tl_member.php

--- src/Resources/contao/languages/en/tl_member.php
+++ src/Resources/contao/languages/en/tl_member.php
@@ -5,3 +5,7 @@ $GLOBALS['TL_LANG']['tl_member']['price_tier_legend']         = array('Test Lege
 
 /* Legends */
 $GLOBALS['TL_LANG']['tl_member']['price_tier']         = array('Test Field', 'This is a test field');
+
+$GLOBALS['TL_LANG']['tl_member']['address_1']         = array('Address 1', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['address_2']         = array('Address 2', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['type_of_practitioner'] = array('Type of Practitioner', 'This is a test field');
