51c42a2bee025e55e0279622169e68abc906cbad
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-06 15:22:31 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -5,7 +5,7 @@ foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
     $GLOBALS['TL_DCA']['tl_member']['palettes'][$k] = str_replace('groups;', 'groups;{price_tier_legend},price_tier;', $v);
 }
 
-$GLOBALS['TL_DCA']['tl_member']['fields']['price_tier'] = array
+$GLOBALS['TL_DCA']['tl_member']['fields']['address_1'] = array
 (
     'exclude'                 => true,
     'search'                  => true,
@@ -14,4 +14,30 @@ $GLOBALS['TL_DCA']['tl_member']['fields']['price_tier'] = array
     'inputType'               => 'text',
     'eval'                    => array('mandatory'=>true, 'maxlength'=>255, 'feEditable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
     'sql'                     => "varchar(255) NOT NULL default ''"
+);
+$GLOBALS['TL_DCA']['tl_member']['fields']['address_2'] = array
+(
+    'exclude'                 => true,
+    'search'                  => true,
+    'sorting'                 => true,
+    'flag'                    => DataContainer::SORT_INITIAL_LETTER_ASC,
+    'inputType'               => 'text',
+    'eval'                    => array('mandatory'=>true, 'maxlength'=>255, 'feEditable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
+);
+
+
+
+
+
+$GLOBALS['TL_DCA']['tl_member']['fields']['address_2'] = array
+(
+    'exclude'                 => true,
+    'search'                  => true,
+    'sorting'                 => true,
+    'flag'                    => DataContainer::SORT_INITIAL_LETTER_ASC,
+    'inputType'                => 'checkbox',
+    'options'                  => array('test_1' => 'Test 1', 'test_2' => 'Test 2'),
+    'eval'                     => array('feEditable'=>true, 'feGroup'=>'personal', 'tl_class'=>'w50'),
+    'sql'                      => "varchar(32) NOT NULL default ''"
 );
