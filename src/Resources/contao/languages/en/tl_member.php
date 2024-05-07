52acab2aba4a7da516aafcf9f6361c4832ef62ba
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-07 08:50:19 -0400

    Update tl_member.php

--- src/Resources/contao/languages/en/tl_member.php
+++ src/Resources/contao/languages/en/tl_member.php
@@ -6,6 +6,18 @@ $GLOBALS['TL_LANG']['tl_member']['price_tier_legend']         = array('Test Lege
 /* Legends */
 $GLOBALS['TL_LANG']['tl_member']['price_tier']         = array('Test Field', 'This is a test field');
 
+// Text Fields
 $GLOBALS['TL_LANG']['tl_member']['address_1']         = array('Address 1', 'This is a test field');
 $GLOBALS['TL_LANG']['tl_member']['address_2']         = array('Address 2', 'This is a test field');
+
+
+// Textarea fields
+$GLOBALS['TL_LANG']['tl_member']['specialties_or_interest']         = array('Address 1', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['how_hear_about_moss']         = array('Address 1', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['besst_time_for_call']         = array('Address 1', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['further_comments']         = array('Address 1', 'This is a test field');
+
+// Checkbox Fields
 $GLOBALS['TL_LANG']['tl_member']['type_of_practitioner'] = array('Type of Practitioner', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['documents_how_to_provide'] = array('Type of Practitioner', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['reason_for_wanting_account'] = array('Type of Practitioner', 'This is a test field');
