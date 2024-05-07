fbe60aeceeaef39d22b8face9714845d4e78e795
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-07 09:23:02 -0400

    Update tl_member.php

--- src/Resources/contao/languages/en/tl_member.php
+++ src/Resources/contao/languages/en/tl_member.php
@@ -12,7 +12,7 @@ $GLOBALS['TL_LANG']['tl_member']['address_2']         = array('Address 2', 'This
 
 
 // Textarea fields
-$GLOBALS['TL_LANG']['tl_member']['specialties_or_interest']         = array('Address 1', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['specialties_or_interest']         = array('Describe your specialties or field of interest as a health care practitioner', 'This is a test field');
 $GLOBALS['TL_LANG']['tl_member']['how_hear_about_moss']         = array('Address 1', 'This is a test field');
 $GLOBALS['TL_LANG']['tl_member']['besst_time_for_call']         = array('Address 1', 'This is a test field');
 $GLOBALS['TL_LANG']['tl_member']['further_comments']         = array('Address 1', 'This is a test field');
@@ -21,3 +21,6 @@ $GLOBALS['TL_LANG']['tl_member']['further_comments']         = array('Address 1'
 $GLOBALS['TL_LANG']['tl_member']['type_of_practitioner'] = array('Type of Practitioner', 'This is a test field');
 $GLOBALS['TL_LANG']['tl_member']['documents_how_to_provide'] = array('Type of Practitioner', 'This is a test field');
 $GLOBALS['TL_LANG']['tl_member']['reason_for_wanting_account'] = array('Type of Practitioner', 'This is a test field');
+
+// Radio Fields
+$GLOBALS['TL_LANG']['tl_member']['verification_of_licensure'] = array('Verification of your licensure, certification, professional degree or relevant student status is required. Please indicate how you will provide this documentation (choose one)', 'This is a test field');
