6d8aaa5af8cdda4c3bc76c0e1e44533399c98e27
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-07 09:25:25 -0400

    Update tl_member.php

--- src/Resources/contao/languages/en/tl_member.php
+++ src/Resources/contao/languages/en/tl_member.php
@@ -13,14 +13,14 @@ $GLOBALS['TL_LANG']['tl_member']['address_2']         = array('Address 2', 'This
 
 // Textarea fields
 $GLOBALS['TL_LANG']['tl_member']['specialties_or_interest']         = array('Describe your specialties or field of interest as a health care practitioner', 'This is a test field');
-$GLOBALS['TL_LANG']['tl_member']['how_hear_about_moss']         = array('Address 1', 'This is a test field');
-$GLOBALS['TL_LANG']['tl_member']['besst_time_for_call']         = array('Address 1', 'This is a test field');
-$GLOBALS['TL_LANG']['tl_member']['further_comments']         = array('Address 1', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['how_hear_about_moss']         = array('How did you hear about Moss Nutrition? (Please be specific. For example, if you heard about us from a friend or colleague, tell us their name. If you did a Google search, what were you looking for? If you heard of us at a class, conference or seminar, note the institution or event)', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['besst_time_for_call']         = array('The best time for us to telephone you during business hours', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['further_comments']         = array('Please add further comments here', 'This is a test field');
 
 // Checkbox Fields
 $GLOBALS['TL_LANG']['tl_member']['type_of_practitioner'] = array('Type of Practitioner', 'This is a test field');
 $GLOBALS['TL_LANG']['tl_member']['documents_how_to_provide'] = array('Type of Practitioner', 'This is a test field');
-$GLOBALS['TL_LANG']['tl_member']['reason_for_wanting_account'] = array('Type of Practitioner', 'This is a test field');
+$GLOBALS['TL_LANG']['tl_member']['reason_for_wanting_account'] = array('What is your primary reason for wanting a Practitioner Account? Choose one', 'This is a test field');
 
 // Radio Fields
 $GLOBALS['TL_LANG']['tl_member']['verification_of_licensure'] = array('Verification of your licensure, certification, professional degree or relevant student status is required. Please indicate how you will provide this documentation (choose one)', 'This is a test field');
