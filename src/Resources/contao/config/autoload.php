58307a63f659f33798ff4a30e59fd6e5a9559ff8
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-07 14:15:18 -0400

    Create autoload.php

---
+++ src/Resources/contao/config/autoload.php
@@ -0,0 +1,20 @@
+<?php
+ 
+/**
+ * Zyppy Popup
+ *
+ * Copyright (C) 2018-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/zyppy_popup
+ * @link       https://andrewstevens.consulting
+ */
+
+ 
+ 
+/**
+ * Register the classes
+ */
+ClassLoader::addClasses(array
+(
+  'Bcs\Handler' 			=> 'system/modules/moss_registration/library/Bcs/Handler.php'
+));
