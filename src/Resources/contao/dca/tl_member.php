ca45419e7d68af3d582df2b4ce012fa20156c08a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2024-05-14 09:21:44 -0400

    Update tl_member.php

--- src/Resources/contao/dca/tl_member.php
+++ src/Resources/contao/dca/tl_member.php
@@ -8,7 +8,12 @@ foreach ($GLOBALS['TL_DCA']['tl_member']['palettes'] as $k => $v) {
 // Convert existing fields to mandatory
 $GLOBALS['TL_DCA']['tl_member']['fields']['city'] = array
 (
-    'eval'                    => array('mandatory'=>true),
+    'filter'                  => true,
+    'search'                  => true,
+    'sorting'                 => true,
+    'inputType'               => 'text',
+    'eval'                    => array('mandatory'=>true, 'maxlength'=>255, 'feEditable'=>true, 'feGroup'=>'address', 'tl_class'=>'w50'),
+    'sql'                     => "varchar(255) NOT NULL default ''"
 );
 
 // Text Fields
